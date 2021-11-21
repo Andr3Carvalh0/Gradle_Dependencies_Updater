a578a2b541245707da4cfe971ec7d352d83031a3
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-11-21 16:29:01 +0100

    Initial work on the Dependabot gradle plugin

---
+++ plugin/src/main/java/pt/andre/DependabotPlugin.kt
@@ -0,0 +1,10 @@
+package pt.andre
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DependabotPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+}
